6d00e8517fb18061e21270388b8e70f3bafc7f45
arjunadhikary <reasonworld12345@gmail.com>
2022-10-13 09:48:32 +0545

    changed the minimum amount of eth required to enter

--- hardhat-helper.config.js
+++ hardhat-helper.config.js
@@ -2,7 +2,7 @@ const { ethers } = require("hardhat")
 
 const developmentChain = ["hardhat", "localhost"]
 const otherConfig = {
-  minimumAmountToEnter: ethers.utils.parseEther("0.1"),
+  minimumAmountToEnter: ethers.utils.parseEther("0.01"),
   gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
   updateInterval: "30",
   callbackGasLimit: "2500000",
